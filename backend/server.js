1a6c71a7186755347bc4a3010a4bd9313a0e75c0
Nikita <kozlov220101@gmail.com>
2023-01-12 12:22:14 +0500

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv").config() 
+const express = require("express")
+const mongoose = require("mongoose")
+const taskRoutes = require("./routes/taskRoute")
+const cors = require("cors")
+
+
+const app = express()
+
+//Middleware
+app.use(express.json())
+//app.use(express.urlencoded({extended: false}))// for form-data
+app.use(cors({
+    origin: ["http://localhost:3000",
+    "https://pet-task-app-mern.onrender.com"]
+}))
+app.use("/api/tasks", taskRoutes)// Позволяет использовать routes из файла task route
+
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home page")
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    }))
+    .catch((err)=> console.log(err))
